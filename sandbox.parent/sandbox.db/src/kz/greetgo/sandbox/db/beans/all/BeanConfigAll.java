520459485859d151b30de0b2243b9fa94492b424
ekolpakov <ekolpakov@greet-go.com>
2017-09-21 16:35:26 +0600

    Настроен запуск тестов

--- sandbox.parent/sandbox.db/src/kz/greetgo/sandbox/db/beans/all/BeanConfigAll.java
+++ sandbox.parent/sandbox.db/src/kz/greetgo/sandbox/db/beans/all/BeanConfigAll.java
@@ -2,7 +2,10 @@ package kz.greetgo.sandbox.db.beans.all;
 
 import kz.greetgo.depinject.core.BeanConfig;
 import kz.greetgo.depinject.core.BeanScanner;
+import kz.greetgo.depinject.core.Include;
+import kz.greetgo.sandbox.db.register_impl.BeanConfigRegisterImpl;
 
 @BeanConfig
 @BeanScanner
+@Include({BeanConfigRegisterImpl.class})
 public class BeanConfigAll {}
---
+++ sandbox.parent/sandbox.db/src/kz/greetgo/sandbox/db/register_impl/BeanConfigRegisterImpl.java
@@ -0,0 +1,8 @@
+package kz.greetgo.sandbox.db.register_impl;
+
+import kz.greetgo.depinject.core.BeanConfig;
+import kz.greetgo.depinject.core.BeanScanner;
+
+@BeanConfig
+@BeanScanner
+public class BeanConfigRegisterImpl {}
