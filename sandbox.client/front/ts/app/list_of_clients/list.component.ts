92df54941d6a7db5b9204b5c18be7fbe71b1e595
Golib98 <golib98@mail.ru>
2017-12-01 19:04:43 +0600

    Added modal form

--- sandbox.client/front/ts/app/list_of_clients/list.component.ts
+++ sandbox.client/front/ts/app/list_of_clients/list.component.ts
@@ -1,27 +1,41 @@
 import {Component, EventEmitter, OnInit, Output} from "@angular/core";
-import {ClientRecord} from "../../model/ClientRecord.ts"
+import {ClientRecord} from "../../model/ClientRecord"
 import {HttpService} from "../HttpService";
 @Component({
   selector: 'list-component',
   template: require('./list-component.html'),
-  styles: [require('./list-component.css')],
+  styles: [require('./list.component.css')],
 })
 export class ListComponent implements OnInit{
   @Output() exit = new EventEmitter<void>();
   loading:boolean = true;
   errorLoading:boolean = false;
+  modalChangeForm:boolean = false;
   list: ClientRecord[] = [];
+  clientToChange:ClientRecord = null;
+
+  openModalChangeForm(id:string){
+    this.modalChangeForm = true;
+    this.clientToChange = this.list.find(i => i.id === id);
+  }
+
+  closeModalChangeForm(){
+    this.modalChangeForm = false;
+  }
+
+
   constructor(private httpService: HttpService) {}
-    ngOnInit(): void {
-      this.loadList();
-      this.loading = false;
-    }
-    loadList(){
-      this.httpService.get("/client/getList").toPromise().then(result => {
-          this.list = result.json().map(ClientRecord.copy);
-      }, error => {
-          this.errorLoading = true;
-          console.log(error);
-      });
-    }
+  ngOnInit(): void {
+    this.loadList();
+    this.loading = false;
+  }
+
+  loadList(){
+    this.httpService.get("/client/getList").toPromise().then(result => {
+        this.list = result.json().map(ClientRecord.copy);
+    }, error => {
+        this.errorLoading = true;
+        console.log(error);
+    });
+  }
 }
