8c1bf84b42ebea3785e7a786b3bc59cf0362a902
Golib98 <golib98@mail.ru>
2017-12-07 13:34:37 +0600

    Корректировка работы со стенд сервером

---
+++ sandbox.controller/src/kz/greetgo/sandbox/controller/model/ClientToSave.java
@@ -0,0 +1,10 @@
+package kz.greetgo.sandbox.controller.model;
+
+
+public class ClientToSave {
+  public String id;
+  public String name;
+  public String surname;
+  public String patronymic;
+  public String phone;
+}
