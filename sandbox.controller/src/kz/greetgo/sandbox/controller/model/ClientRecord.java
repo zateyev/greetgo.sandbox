72737aef06efd46c41d7f4c0ec64ecea0396ecde
ndemina <ndemina@greet-go.com>
2017-12-20 12:48:15 +0600

    Добавила поля клиенту, создала в базе данных модель ClientDot

--- sandbox.controller/src/kz/greetgo/sandbox/controller/model/ClientRecord.java
+++ sandbox.controller/src/kz/greetgo/sandbox/controller/model/ClientRecord.java
@@ -6,4 +6,8 @@ public class ClientRecord {
   public String id;
   public String fio;
   public String charm;
+  public int age;
+  public int totalBalance;
+  public int maxBalance;
+  public int minBalance;
 }
---
+++ sandbox.stand/db/src/kz/greetgo/sandbox/db/stand/model/ClientDot.java
@@ -0,0 +1,23 @@
+package kz.greetgo.sandbox.db.stand.model;
+
+
+import kz.greetgo.sandbox.controller.model.ClientRecord;
+
+public class ClientDot {
+  public String id, fio,  charm;
+  public int age,  totalBalance,  maxBalance, minBalance;
+
+  public ClientRecord toClientRecord(){
+    ClientRecord ret = new ClientRecord();
+    ret.id = id;
+    ret.fio = fio;
+    ret.charm = charm;
+    ret.age = age;
+    ret.totalBalance = totalBalance;
+    ret.maxBalance = maxBalance;
+    ret.minBalance = minBalance;
+    return ret;
+  }
+
+
+}
