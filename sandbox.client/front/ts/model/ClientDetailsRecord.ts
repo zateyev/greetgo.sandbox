a779048bfc795ccaaffa89e81fc589cd0e1d4632
ndemina <ndemina@greet-go.com>
2017-12-25 15:49:57 +0600

    Добавление детализированной модели клиента

---
+++ sandbox.client/front/ts/model/ClientDetailsRecord.ts
@@ -0,0 +1,17 @@
+export class ClientDetailsRecord{
+  public surname: string;
+  public name: string;
+  public patronymic: string;
+  public gender: string;
+  public birth_date: string;
+  public charm: string;
+  public factAddrStreet: string;
+  public factAddrHouse: number;
+  public factAddrFlat: number;
+  public regAddrStreet: string;
+  public regAddrHouse: number;
+  public regAddrFlat: number;
+  public homePhone: string;
+  public workPhone: string;
+  public mobilePhone: string;
+}
