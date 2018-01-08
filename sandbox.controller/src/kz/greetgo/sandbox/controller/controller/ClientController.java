2516235e3404bd73e6579b19f00edda9bed6e673
nsmagulov <nsmagulov@greet-go.com>
2018-01-08 13:45:30 +0600

    Созданы пустые классы для начала программирвагния

---
+++ sandbox.controller/src/kz/greetgo/sandbox/controller/controller/ClientController.java
@@ -0,0 +1,23 @@
+package kz.greetgo.sandbox.controller.controller;
+
+import kz.greetgo.depinject.core.Bean;
+import kz.greetgo.depinject.core.BeanGetter;
+import kz.greetgo.mvc.annotations.*;
+import kz.greetgo.sandbox.controller.model.AuthInfo;
+import kz.greetgo.sandbox.controller.model.UserInfo;
+import kz.greetgo.sandbox.controller.register.AuthRegister;
+import kz.greetgo.sandbox.controller.security.NoSecurity;
+import kz.greetgo.sandbox.controller.util.Controller;
+
+/**
+ * как составлять контроллеры написано
+ * <a href="https://github.com/greetgo/greetgo.mvc/blob/master/greetgo.mvc.parent/doc/controller_spec.md">здесь</a>
+ */
+@Bean
+@Mapping("/client")
+public class ClientController implements Controller {
+  @Mapping("/asd")
+  public void asd() {
+
+  }
+}
