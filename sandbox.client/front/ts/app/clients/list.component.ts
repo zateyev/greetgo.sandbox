3099199c449866f35f2466e71aac946c97bbe9be
Golib98 <golib98@mail.ru>
2018-01-08 12:15:11 +0600

    download в отдельный контроллер

--- sandbox.client/front/ts/app/clients/list.component.ts
+++ sandbox.client/front/ts/app/clients/list.component.ts
@@ -119,7 +119,7 @@ export class ListComponent implements OnInit {
     let listInfo = encodeURIComponent(JSON.stringify(this.listInfo));
     let contentType = this.fileTypeForDownload;
     let token = encodeURIComponent(this.httpService.token);
-    window.open(this.httpService.url("/client/downloadReport?contentType=" + contentType
+    window.open(this.httpService.url("/report/clientList?contentType=" + contentType
       + "&listInfo=" + listInfo + "&token=" + token));
   }
 
--- sandbox.controller/src/kz/greetgo/sandbox/controller/controller/ClientController.java
+++ sandbox.controller/src/kz/greetgo/sandbox/controller/controller/ClientController.java
@@ -2,8 +2,10 @@ package kz.greetgo.sandbox.controller.controller;
 
 import kz.greetgo.depinject.core.Bean;
 import kz.greetgo.depinject.core.BeanGetter;
-import kz.greetgo.mvc.annotations.*;
-import kz.greetgo.mvc.interfaces.RequestTunnel;
+import kz.greetgo.mvc.annotations.Json;
+import kz.greetgo.mvc.annotations.Mapping;
+import kz.greetgo.mvc.annotations.Par;
+import kz.greetgo.mvc.annotations.ToJson;
 import kz.greetgo.sandbox.controller.model.ClientDetails;
 import kz.greetgo.sandbox.controller.model.ClientListRequest;
 import kz.greetgo.sandbox.controller.model.ClientRecord;
@@ -11,9 +13,6 @@ import kz.greetgo.sandbox.controller.model.ClientToSave;
 import kz.greetgo.sandbox.controller.register.ClientRegister;
 import kz.greetgo.sandbox.controller.util.Controller;
 
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 
@@ -42,34 +41,6 @@ public class ClientController implements Controller {
   @Mapping("/getClient")
   public ClientDetails getClient(@Par("id") String id) {
     return clientRegister.get().getClient(id);
-  }
-
-
-  @Mapping("/downloadReport")
-  public void downloadReport(@Par("listInfo") @Json ClientListRequest clientListRequest,
-                             @Par("contentType") String contentType,
-                             @ParSession("personId") String personId,
-                             RequestTunnel tunnel
-  ) throws Exception {
-
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-    String date = sdf.format(new Date());
-
-    tunnel.setResponseContentType(contentType);
-
-    OutputStream outputStream = tunnel.getResponseOutputStream();
-
-    if (contentType.contains("pdf")) {
-      tunnel.setResponseHeader("content-disposition", "attachment; filename=\"List_of_clients-" + date + ".pdf\"");
-    } else {
-      tunnel.setResponseHeader("content-disposition", "attachment; filename=\"List_of_clients-" + date + ".xlsx\"");
-    }
-
-    clientRegister.get().downloadReport(clientListRequest, outputStream, contentType, personId);
-
-    tunnel.flushBuffer();
-
   }
 
   @ToJson
