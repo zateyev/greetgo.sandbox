63757425a9e497bc52d848430f17d3bc0059e22a
zateyev <zhasulan.ateyev@gmail.com>
2018-03-29 08:20:45 +0600

    mini refactoring
    mini refactoring

--- sandbox.db/src/kz/greetgo/sandbox/db/migration_impl/Migration.java
+++ sandbox.db/src/kz/greetgo/sandbox/db/migration_impl/Migration.java
@@ -1,7 +1,6 @@
 package kz.greetgo.sandbox.db.migration_impl;
 
 import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.SftpException;
 import kz.greetgo.depinject.core.Bean;
 import kz.greetgo.depinject.core.BeanGetter;
 import kz.greetgo.sandbox.db.configs.DbConfig;
@@ -10,7 +9,6 @@ import kz.greetgo.sandbox.db.migration_impl.report.ReportXlsx;
 import kz.greetgo.sandbox.db.ssh.InputFileWorker;
 import kz.greetgo.sandbox.db.ssh.LocalFileWorker;
 import kz.greetgo.sandbox.db.ssh.SshConnection;
-import kz.greetgo.sandbox.db.util.TimeUtils;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
@@ -18,8 +16,6 @@ import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 @Bean
@@ -95,62 +91,6 @@ public class Migration {
       }
     }
   }
-
-//  public void executeCiaFrsMigrationConcurrently() throws Exception {
-//
-//    File outErrorFile = new File(migrationConfig.get().inFilesHomePath() + migrationConfig.get().outErrorFileName());
-//    outErrorFile.getParentFile().mkdirs();
-//
-//    File file = new File(migrationConfig.get().sqlReportDir() + "sqlReportCia.xlsx");
-//    file.getParentFile().mkdirs();
-//    ReportXlsx reportXlsx = new ReportXlsx(new FileOutputStream( file));
-//    reportXlsx.start();
-//
-//    File fileFrs = new File(migrationConfig.get().sqlReportDir() + "sqlReportFrs.xlsx");
-//    fileFrs.getParentFile().mkdirs();
-//    ReportXlsx reportXlsxFrs = new ReportXlsx(new FileOutputStream( fileFrs));
-//    reportXlsxFrs.start();
-//
-//    try (
-//      Connection connectionForCia = getConnection();
-//      Connection connectionForFrs = getConnection();
-//      InputFileWorker inputFileWorker = getInputFileWorker()
-//    ) {
-//      long startedAt = System.nanoTime();
-//
-//      CiaMigrationWorker ciaMigrationWorker = new CiaMigrationWorker(connectionForCia);
-//      ciaMigrationWorker.outErrorFile = outErrorFile;
-//      ciaMigrationWorker.reportXlsx = reportXlsx;
-//      ciaMigrationWorker.maxBatchSize = migrationConfig.get().maxBatchSize();
-//
-//      FrsMigrationWorker frsMigrationWorker = new FrsMigrationWorker(connectionForFrs);
-////      frsMigrationWorker.outErrorFile = outErrorFile;
-//      frsMigrationWorker.reportXlsx = reportXlsxFrs;
-//      frsMigrationWorker.maxBatchSize = migrationConfig.get().maxBatchSize();
-//
-//      final Thread frsDownloading = new Thread(() -> {
-//        try {
-//          frsMigrationWorker.createTmpTables();
-//          frsMigrationWorker.parseDataAndSaveInTmpDb();
-//          frsMigrationWorker.validateErrors();
-//        } catch (SQLException | SftpException | IOException e) {
-//          e.printStackTrace();
-//        }
-//      });
-//      frsDownloading.start();
-//      ciaMigrationWorker.migrate();
-//      frsDownloading.join();
-//
-//      frsMigrationWorker.migrateFromTmp();
-//
-//      long now = System.nanoTime();
-//      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-//      System.out.println(sdf.format(new Date()) + " [FRS and CIA] TOTAL migration ended for time " + TimeUtils.showTime(now, startedAt) + " s");
-//    } finally {
-//      reportXlsx.finish();
-//      reportXlsxFrs.finish();
-//    }
-//  }
 
   private InputStream extractFromFile(InputStream inputStream) throws IOException {
     BZip2CompressorInputStream bZip2CompressorIS = new BZip2CompressorInputStream(inputStream);
